925d85fe84b78653fa9238764def3845595d865f
Vyronevra-dev <pabloevra27@gmail.com>
2026-07-08 11:56:24 +0300

    first commit

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ burger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
     burger.classList.toggle('open');
 });
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 20) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
